6b184125eb52f2c559e1cfcc64568d28325e2fba
Anton <Lantar23@yandex.ru>
2017-11-15 01:06:03 +0300

    упрощён шаблон и стили строки добавления

--- src/app/formulas/adding-pane/adding-pane.component.ts
+++ src/app/formulas/adding-pane/adding-pane.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgForm, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { ChannelService } from '../../channel.service';
 import { InfoType } from '../../info-type.enum';
@@ -11,50 +11,51 @@ import { InfoType } from '../../info-type.enum';
 })
 export class AddingPaneComponent {
   viewState = 'initial';
-  main: FormGroup;
+
+  type: FormGroup;
+  variable: FormGroup;
+  sym: FormGroup;
+  axiom: FormGroup;
 
   constructor(private fb: FormBuilder, private channel: ChannelService) {
-    this.main = fb.group({
-      initial: new FormGroup({}),
-      type: fb.group({
-        typeName: ['', Validators.required]
-      }),
-      variable: fb.group({
-        varName: '',
-        typeName: ''
-      }),
-      sym: fb.group({
-        symName: '',
-        arg1Type: '', // todo добавить кнопку добавления инпутов
-        retType: ''
-      }),
-      axiom: fb.group({
-        axiom: ''
-      })
+    this.type = fb.group({
+      typeName: ['', Validators.required]
+    });
+    this.variable = fb.group({
+      varName: '',
+      typeName: ''
+    });
+    this.sym = fb.group({
+      symName: '',
+      arg1Type: '', // todo добавить кнопку добавления инпутов
+      retType: ''
+    });
+    this.axiom = fb.group({
+      axiom: ''
     });
   }
 
   addType(): void {
     this.channel.send(InfoType.Text,
-      'add_type ' + this.main.get('type.typeName').value);
+      'add_type ' + this.type.get('typeName').value);
     this.viewState = 'initial';
   }
   addVar(): void {
     this.channel.send(InfoType.Text, 'add_var ' +
-      this.main.get('variable.varName').value + ' ' +
-      this.main.get('variable.typeName').value);
+      this.variable.get('varName').value + ' ' +
+      this.variable.get('typeName').value);
     this.viewState = 'initial';
   }
   addSym(): void {
     this.channel.send(InfoType.Text, 'add_sym ' +
-      this.main.get('sym.symName').value + ' ' +
-      this.main.get('sym.arg1Type').value + ' ' +
-      this.main.get('sym.retType').value);
+      this.sym.get('symName').value + ' ' +
+      this.sym.get('arg1Type').value + ' ' +
+      this.sym.get('retType').value);
     this.viewState = 'initial';
   }
   addAxiom(): void {
     this.channel.send(InfoType.Text, 'add_axiom ' +
-      this.main.get('axiom.axiom').value);
+      this.axiom.get('axiom').value);
     this.viewState = 'initial';
   }
 }
